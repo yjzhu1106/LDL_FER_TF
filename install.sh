86457f32edc696fa90b214e43660100e2a89bb1c
yjzhu1106 <yjzhu1106@163.com>
2023-01-01 23:27:02 +0800

    init empty

--- install.sh
+++
@@ -1,4 +0,0 @@
-conda install -c conda-forge tensorflow-gpu==2.6.0 -y
-pip install pandas opencv-python scikit-learn matplotlib
-pip install git+https://github.com/qubvel/classification_models.git
-
